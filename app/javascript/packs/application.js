24cff6ce08ff19fb55289cea9dde5722ed1c14e0
Thanoja Thangaraja <thanujathanuja18@yahoo.com>
2021-03-02 17:10:24 +0000

    modified sessions file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import { LoginToggle } from '../components/login_form';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -31,4 +32,8 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  if (document.querySelector('.sign-in')) {
+    console.log('sign in found');
+    LoginToggle();
+  }
 });
