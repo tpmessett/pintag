84b35a79b6c284e0c4b470925dae27dd6496aa94
thomas messett <thomas.messett@stampapp.me>
2021-03-03 17:15:19 +0000

    fixed tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,5 +3,5 @@ class Tag < ApplicationRecord
   has_many :contents, through: :content_tags
   belongs_to :user
   include PgSearch::Model
-  multisearchable against: [name: 'A'], using: [:tsearch]
+  multisearchable against: [:name], using: [:tsearch]
 end
