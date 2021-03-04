01182790d8f1187372bf69b77280aa46b9eefbc4
Nathan Ipale <nathanipale@gmail.com>
2021-03-04 15:36:15 +0000

    avatars done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :board_permissions
   has_many :boards
   has_many :tags
+  has_one_attached :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
