1c7898b5fa84ce5acc2be66c2390777d1e650ed1
Dan Menza <dansmenza@gmail.com>
2021-03-08 15:30:01 +0000

    multi-tag select and create from content page

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -11,12 +11,14 @@ class ContentsController < ApplicationController
     @content = Content.new(content_params)
     @content.board = @board
     authorize @content
-    tags = params[:content][:tags].reject(&:blank?).map(&:to_i)
-    @content.tags = Tag.where(id: tags)
-    if @content.save
-      redirect_to board_path(@board)
-    else
-      render :new
+    tags = find_or_create_tag
+    if Tag.where(id: tags)
+      @content.tags = Tag.where(id: tags)
+      if @content.save
+        redirect_to board_path(@board)
+      else
+        render :new
+      end
     end
   end
 
@@ -35,6 +37,8 @@ class ContentsController < ApplicationController
   end
 
   def edit
+    policy_scope(Tag)
+    @tags = Tag.all
     find_content
     authorize @content
   end
@@ -42,6 +46,8 @@ class ContentsController < ApplicationController
   def update
     find_content
     authorize @content
+    tags = find_or_create_tag
+    @content.tags = Tag.where(id: tags)
     @content.update(content_params)
     redirect_to board_path(@content.board)
   end
@@ -49,6 +55,27 @@ class ContentsController < ApplicationController
   def find_content
     @content = Content.find(params[:id])
   end
+
+  def find_or_create_tag
+    tags = []
+    if params[:content][:tags].present?
+      input_tags = params[:content][:tags].reject(&:blank?)
+    else
+      input_tags = params[:content][:tag_ids].reject(&:blank?)
+    end
+    input_tags.each do |input|
+      tag = Tag.where(id: input.to_i).first
+      if tag.present?
+        tags << tag.id
+      else
+        new_tag = Tag.create(name: input, user: current_user)
+        tags << new_tag.id
+      end
+    end
+    return tags
+  end
+
+  private
 
   def content_params
     params.require(:content).permit(:name, :description, :link, :file_upload)
